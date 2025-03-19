032638799db337092e7e7b7ec667c4636fd1c3f9
Chris Aaron Abesamis <aaronabesamis14@gmail.com>
2025-03-19 10:17:12 +0800

    database

--- database/book-borrowing.sql
+++ database/book-borrowing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2025 at 10:25 AM
+-- Generation Time: Mar 19, 2025 at 03:16 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
