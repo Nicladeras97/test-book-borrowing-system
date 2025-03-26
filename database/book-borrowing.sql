1bac08a909b7bdd05232b1bb979d3cf7ba36c90a
rhea martin <rheamartin56@gmail.com>
2025-03-26 13:08:10 +0800

    update database

--- database/book-borrowing.sql
+++ database/book-borrowing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2025 at 01:04 PM
+-- Generation Time: Mar 26, 2025 at 06:06 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,59 +42,6 @@ CREATE TABLE `book` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `book`
---
-
-INSERT INTO `book` (`Title`, `Author`, `Year`, `ISBN`, `Category`, `Status`, `Image`, `Copies`, `AddedDate`, `CallNumber`, `RackNumber`) VALUES
-('Philippine Journal of Science', 'Department of Science and Technology', 1906, '0031-7683', 'Journal', 'Borrowed', NULL, 1, '2025-03-25', 'JNL PJS 1906', 5),
-('How to Write a Thesis', 'Umberto Eco', 1977, '262527138', 'Thesis', 'Available', NULL, 1, '2025-03-25', 'THE ECO 1977', 6),
-('A Concise Guide to Writing a Thesis or Dissertation', 'Halyna M. Kornuta and Ron W. Germaine', 2019, '429615000', 'Thesis', 'Available', NULL, 1, '2025-03-25', 'THE KOR 2019', 6),
-('Yellowface', 'R.F Kuang', 2023, '978-0-008532-77-2', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC KUA 2023', 1),
-('The Poetry Of Robert Frost', 'Robert Frost', 1969, '978-0-030725-35-7', 'Non-Fiction', 'Borrowed', NULL, 1, '2025-03-25', 'NF FRO 1969', 2),
-('Engineering Mechanics: Statics', 'Russell C. Hibbeler', 2019, '978-0-134686-60-1', 'TextBook', 'Available', NULL, 1, '2025-03-25', 'TB HIB 2019', 4),
-('The Tao Of Pooh', 'Benjamin Hoff', 1983, '978-0-140067-47-7', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF HOF 1983', 2),
-('How to Write a BA Thesis, Second Edition', 'Charles Lipson', 2018, '978-0-22643107-9', 'Thesis', 'Available', NULL, 1, '2025-03-25', 'THE LIP 2018', 6),
-('Writing Your Journal Article in Twelve Weeks', 'Wendy Laura Belcher', 2019, '978-0-226499-91-8', 'Journal', 'Borrowed', NULL, 1, '2025-03-25', 'JNL BEL 2019', 5),
-('Born To Run', 'Christopher McDougall', 2009, '978-0-307266-30-9', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF MCD 2009', 2),
-('Quiet', 'Susan Cain', 2012, '978-0-307352-14-9', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF CAI 2012', 2),
-('Bird\'s Basic Engineering Mathematics', 'John Bird', 2021, '978-0-367643-67-6', 'TextBook', 'Available', NULL, 1, '2025-03-25', 'TB BIR 2021', 4),
-('The Diary Of A Young Girl', 'Anne Frank', 1995, '978-0-385473-78-1', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF FRA 1995', 2),
-('Born A Crime', 'Trevor Noah', 2016, '978-0-399588-17-4', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF NOA 2016', 2),
-('The Laws of Human Nature', 'Robert Greene', 2018, '978-0-525428-14-5', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF GRE 2018', 2),
-('The Midnight Library', 'Matt Haig', 2020, '978-0-525559-47-4', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC HAI 2020', 1),
-('Sword Catcher (The Chronicles of Castellane)', 'Cassandra Clare', 2023, '978-0-525620-01-3', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC CLA 2023', 1),
-('Calculus', 'James Stewart', 2016, '978-0-538497-90-9', 'TextBook', 'Available', NULL, 1, '2025-03-25', 'TB STE 2016', 4),
-('Black Woods Blue Sky', 'Eowyn Ivey', 2025, '978-0-593231-02-9', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC IVE 2025', 1),
-('A Room Of One\'s Own', 'Virginia Woolf', 1989, '978-0-606208-90-1', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF WOO 1989', 2),
-('Silent Spring', 'Rachel Carson', 1962, '978-0-618253-05-0', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF CAR 1962', 2),
-('Through Time Into Healing', 'Brian L. Weiss', 1992, '978-0-671867-86-7', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF WEI 1992', 2),
-('River of Blue', 'Tad William', 1998, '978-0-886777-77-7', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF WIL 1998', 2),
-('Talk English', 'Ken Xiao', 2016, '978-0-998163-20-8', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF XIA 2016', 2),
-('Science Fiction and Fantasy Literature', 'R.Reginald', 2010, '978-0941028-76-9', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC REG 2010', 1),
-('An Exploration of Sustainable Energy Solutions', 'Emily Johnson', 2022, '978-1-234-567-89-0', 'Thesis', 'Available', NULL, 1, '2025-03-25', 'THE JOH 2022', 6),
-('Principles of Economics', 'Nicholas Gregory Mankiw', 2017, '978-1-30558512-6', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC MAN 2017', 1),
-('The Book of Records', 'Madeleine Thien', 2025, '978-1-324078-65-4', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC THI 2025', 1),
-('The aventures of Tom Sawyer', 'Mark Twain', 2004, '978-1-402714-60-3', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC TWA 2004', 1),
-('The Course o True Love', 'Cassandra Clare', 2014, '978-1-442495-63-0', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC CLA 2014', 1),
-('Our Plastic Problem: A Call for Global Solutions', 'Megan Durnford', 2025, '978-1-459836-70-9', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF DUR 2025', 2),
-('I have No Mouth & I Must Scream', 'Harlan Ellison', 2014, '978-1-497609-61-7', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF ELL 2014', 2),
-('All your perfects', 'Colleen Hoover', 2018, '978-1-501171-59-8', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC HOO 2018', 1),
-('I want to die but I want to eat Tteokbokki', 'Baek Sehee', 2018, '978-1-526650-86-3', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF SEE 2018', 2),
-('Lessons in Chemistry', 'Bonnie Garmus', 2022, '978-1-529938-29-6', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC GAR 2022', 1),
-('In Cold Blood', 'Truman Capote', 2001, '978-1-588361-65-3', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF CAP 2001', 2),
-('Finish what you start', 'Peter Hollins', 2018, '978-1-647430-51-1', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF HOL 2018', 2),
-('Blaze Orange: A Midcoast Maine Mystery', 'Allison Keeton', 2025, '978-1-685128-64-7', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC KEE 2025', 1),
-('No-Fail Habits', 'Michael Hyatt', 2020, '978-1-735381-71-8', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF HYA 2020', 2),
-('Self-Publish & Succeed', 'Julie Broad', 2021, '978-1-736031-50-6', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF BRO 2021', 2),
-('Data-Driven Decisions', 'Amy Stubbing', 2022, '978-1-783304-79-0', 'TextBook', 'Available', NULL, 1, '2025-03-25', 'TB STU 2022', 4),
-('Ancient Greek 1; A 21st Century Approach', 'Philip S. Peek', 2021, '978-1-800642-56-0', 'History', 'Available', NULL, 1, '2025-03-25', 'HIS PEE 2021', 3),
-('Atomic Habits', 'James Clear', 2018, '978-1-80422020-7', 'Fiction', 'Available', NULL, 1, '2025-03-25', 'FIC CLE 2018', 1),
-('101 Essays that will Change the way you think', 'Brianna West', 2016, '978-1-945796-06-7', 'Non-Fiction', 'Available', NULL, 1, '2025-03-25', 'NF WES 2016', 2),
-('Atomic Physics for Everyone', 'Will Raven', 2025, '978-3-031695-07-0', 'TextBook', 'Borrowed', NULL, 1, '2025-03-25', 'TB RAV 2025', 4),
-('Faulkner’s Treatment of Women', 'Dr. Vibha Manoj Sharma', 2017, '978-81-93390-41-2', 'Journal', 'Available', NULL, 1, '2025-03-25', 'JNL SHA 2017', 5),
-('Propagation of Glory Lily (Gloriosa superba L.)', 'Dr. S. Anandhi', 2017, '978-81-93390-46-7', 'Journal', 'Available', NULL, 1, '2025-03-25', 'JNL ANA 2017', 5);
-
---
 -- Triggers `book`
 --
 DELIMITER $$
@@ -127,16 +74,6 @@ CREATE TABLE `borrow` (
   `DueDate` date DEFAULT NULL,
   `StatusName` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `borrow`
---
-
-INSERT INTO `borrow` (`BorrowID`, `StudNo`, `ISBN`, `Title`, `CopyID`, `BorrowDate`, `DueDate`, `StatusName`) VALUES
-(1, '211083', '0031-7683', 'Philippine Journal of Science', 0, '2025-03-25', '2025-03-26', 'Borrowed'),
-(2, '211241', '978-0-030725-35-7', 'The Poetry Of Robert Frost', 0, '2025-03-25', '2025-03-26', 'Borrowed'),
-(3, '211241', '978-0-226499-91-8', 'Writing Your Journal Article in Twelve Weeks', 0, '2025-03-25', '2025-03-25', 'Borrowed'),
-(4, '211083', '978-3-031695-07-0', 'Atomic Physics for Everyone', 0, '2025-03-25', '2025-03-25', 'Borrowed');
 
 -- --------------------------------------------------------
 
@@ -197,8 +134,7 @@ CREATE TABLE `return_good` (
   `ISBN` varchar(20) DEFAULT NULL,
   `CopyID` varchar(10) DEFAULT NULL,
   `StudNo` varchar(15) NOT NULL,
-  `ReturnDate` date NOT NULL,
-  `Remarks` text DEFAULT NULL
+  `ReturnDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -214,8 +150,7 @@ CREATE TABLE `return_lost` (
   `StudNo` varchar(50) DEFAULT NULL,
   `ReportDate` date DEFAULT NULL,
   `GracePeriod` date DEFAULT NULL,
-  `FineAmount` decimal(10,2) DEFAULT NULL,
-  `ReturnDate` date DEFAULT NULL
+  `FineAmount` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -321,7 +256,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `borrow`
 --
 ALTER TABLE `borrow`
-  MODIFY `BorrowID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `BorrowID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `login`
@@ -333,7 +268,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `return_damaged`
 --
 ALTER TABLE `return_damaged`
-  MODIFY `ReturnDamagedID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `ReturnDamagedID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `return_good`
@@ -345,7 +280,7 @@ ALTER TABLE `return_good`
 -- AUTO_INCREMENT for table `return_lost`
 --
 ALTER TABLE `return_lost`
-  MODIFY `ReturnLostID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ReturnLostID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
