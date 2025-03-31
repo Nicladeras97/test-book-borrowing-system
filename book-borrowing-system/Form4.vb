b3c92bdee037b3dc6917497b68e8d60f3283f8dd
rhea martin <rheamartin56@gmail.com>
2025-03-31 16:54:23 +0800

    update

--- book-borrowing-system/Form4.vb
+++ book-borrowing-system/Form4.vb
@@ -15,4 +15,15 @@
         Me.Hide()
     End Sub
 
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim returnform As New Form9
+        returnform.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim report As New Report
+        report.Show()
+        Me.Hide()
+    End Sub
 End Class
---
+++ book-borrowing-system/Report.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Report
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label2 = New Label()
+        DataGridView1 = New DataGridView()
+        Button3 = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(472, 35)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(130, 20)
+        Label2.TabIndex = 2
+        Label2.Text = "Borrower's Record"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(32, 108)
+        DataGridView1.Margin = New Padding(3, 4, 3, 4)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(1008, 329)
+        DataGridView1.TabIndex = 9
+        ' 
+        ' Button3
+        ' 
+        Button3.BackgroundImage = My.Resources.Resources.back
+        Button3.BackgroundImageLayout = ImageLayout.Zoom
+        Button3.Cursor = Cursors.Hand
+        Button3.FlatAppearance.BorderSize = 0
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.Location = New Point(32, 25)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(30, 40)
+        Button3.TabIndex = 14
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Report
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1203, 600)
+        Controls.Add(Button3)
+        Controls.Add(DataGridView1)
+        Controls.Add(Label2)
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "Report"
+        Text = "Report"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button3 As Button
+End Class
